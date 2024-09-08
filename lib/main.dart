e57941ef49fcb2e163069f9e9a721a9727f4f727
emirkaganyigit <emirkaganyigit@gmail.com>
2024-09-08 16:54:28 +0300

    security issues fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,23 @@
-import 'package:agrishare/pages/EntrepreneurMain.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:agrishare/pages/LoginRegisterScreen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Firebase yapılandırma detayları
-  const firebaseConfig = FirebaseOptions(
-    apiKey: "AIzaSyD9LjRYZhPrn4bPxAErYDOzRzSXrBo0Rls", // Firebase konsolundan aldığınız bilgilerle değiştirin
-    authDomain: "agrishare-ed20f.firebaseapp.com",
-    projectId: "agrishare-ed20f",
-    storageBucket: "agrishare-ed20f.appspot.com",
-    messagingSenderId: "154275439031",
-    appId: "1:154275439031:android:1b03919fd12e146f080832",
+  // .env dosyasını yükle
+  await dotenv.load();
+
+  // Firebase yapılandırmasını .env dosyasından al
+  final firebaseConfig = FirebaseOptions(
+    apiKey: dotenv.env['FIREBASE_API_KEY']!,
+    authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN']!,
+    projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
+    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
+    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
+    appId: dotenv.env['FIREBASE_APP_ID']!,
   );
 
   await Firebase.initializeApp(
@@ -62,7 +65,7 @@ class _InitialPageState extends State<InitialPage> {
             CircleAvatar(
               radius: 50,
               backgroundColor: Colors.transparent,
-              child: Image.asset('logo.jpg'), // Logo yolu güncellendi
+              child: Image.asset('logo.jpg'), // Logo yolunu doğru belirleyin
             ),
             SizedBox(height: 40),
             CircularProgressIndicator(
